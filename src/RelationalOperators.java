3764403354fde273b21547d6d228c9422d4ac32a
israel david dahan <id@studiodvd.co.il>
2014-12-15 20:39:12 +0200

    add files

---
+++ src/RelationalOperators.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x,y;
+Scanner sc = new Scanner (System.in);
+	System.out.print("please enter number to x: ");
+	x = sc.nextInt();
+	System.out.print("please enter number to y: ");
+	y = sc.nextInt();
+	
+	int sum = x + y ;
+	System.out.print(sum);
+	}
+
+}
